06f39c61d4c87e4c72c93be3ae4178bbe506e74d
Jackson <43098221+SpikedJackson@users.noreply.github.com>
2022-12-02 23:48:26 -0500

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class day2 {
+    public static void main(String[] args) throws Exception {
+        File myObj = new File("src/input2.txt");
+        Scanner myReader = new Scanner(myObj);
+        int total = 0;
+        int total2 = 0;
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            int player = data.charAt(2)-23-64;
+            int elf = data.charAt(0)-64;
+            total+=player;
+            if(elf==player){
+                total+=3;
+            } else if(player==elf+1 || player==elf-2){
+                total+=6;
+            }
+            if(player==2){
+                total2+=3;
+                total2+=elf;
+            } else if(player==3){
+                total2+=6;
+                if(elf!=3){
+                    total2+=elf+1;
+                } else {
+                    total2+=elf-2;
+                }
+            } else {
+                if(elf!=1){
+                    total2+=elf-1;
+                } else {
+                    total2+=elf+2;
+                }
+            }
+        }
+        System.out.println(total);
+        System.out.println(total2);
+    }
+}
